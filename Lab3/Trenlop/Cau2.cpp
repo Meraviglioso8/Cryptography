9968140edd2590302a95d9b5e5ef070ed176158c
Huyen Tran <46748862+Meraviglioso8@users.noreply.github.com>
2023-04-10 15:59:22 +0700

    Add files via upload

---
+++ Lab3/Trenlop/Cau2.cpp
@@ -0,0 +1,113 @@
+//#include "stdafx.h"
+
+#include "rsa.h"
+using CryptoPP::RSA;
+using CryptoPP::RSASS;
+using CryptoPP::InvertibleRSAFunction;
+
+#include "pssr.h"
+using CryptoPP::PSS;
+
+#include "sha.h"
+using CryptoPP::SHA1;
+
+#include "files.h"
+using CryptoPP::FileSink;
+using CryptoPP::FileSource;
+
+#include "osrng.h"
+using CryptoPP::AutoSeededRandomPool;
+
+#include "SecBlock.h"
+using CryptoPP::SecByteBlock;
+
+#include <string>
+using std::string;
+
+#include <iostream>
+using std::cout;
+using std::endl;
+using CryptoPP::byte;
+
+int main(int argc, char* argv[])
+{
+    try
+    {
+        ////////////////////////////////////////////////
+        // Generate keys
+        AutoSeededRandomPool rng;
+
+        InvertibleRSAFunction parameters;
+        parameters.GenerateRandomWithKeySize( rng, 1024 );
+
+        RSA::PrivateKey privateKey( parameters );
+        RSA::PublicKey publicKey( parameters );
+
+        // Message
+        string message = "RSA Encryption Schemes";
+
+        // Signer object
+        RSASS<PSS, SHA1>::Signer signer( privateKey );
+
+        // Create signature space
+        size_t length = signer.MaxSignatureLength();
+        SecByteBlock signature( length );
+
+        // Sign message
+        signer.SignMessage( rng, (const byte*) message.c_str(),
+            message.length(), signature );
+
+        // Verifier object
+        RSASS<PSS, SHA1>::Verifier verifier( publicKey );
+
+        // Verify
+        bool result = verifier.VerifyMessage( (const byte*)message.c_str(),
+            message.length(), signature, signature.size() );
+
+        // Result
+        if( true == result ) {
+            cout << "Signature on message verified" << endl;
+        } else {
+            cout << "Message verification failed" << endl;
+        }
+
+    } // try
+
+    catch( CryptoPP::Exception& e ) {
+        std::cerr << "Error: " << e.what() << std::endl;
+    }
+
+    return 0;
+}
+
+void SaveKey( const RSA::PublicKey& PublicKey, const string& filename )
+{
+    // DER Encode Key - X.509 key format
+    PublicKey.Save(
+        FileSink( filename.c_str(), true /*binary*/ ).Ref()
+    );
+}
+
+void SaveKey( const RSA::PrivateKey& PrivateKey, const string& filename )
+{
+    // DER Encode Key - PKCS #8 key format
+    PrivateKey.Save(
+        FileSink( filename.c_str(), true /*binary*/ ).Ref()
+    );
+}
+
+void LoadKey( const string& filename, RSA::PublicKey& PublicKey )
+{
+    // DER Encode Key - X.509 key format
+    PublicKey.Load(
+        FileSource( filename.c_str(), true, NULL, true /*binary*/ ).Ref()
+    );
+}
+
+void LoadKey( const string& filename, RSA::PrivateKey& PrivateKey )
+{
+    // DER Encode Key - PKCS #8 key format
+    PrivateKey.Load(
+        FileSource( filename.c_str(), true, NULL, true /*binary*/ ).Ref()
+    );
+}
